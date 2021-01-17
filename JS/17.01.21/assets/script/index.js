7fe7fd36c38471ee94f5edbc5b12aa3129cc4617
DzenPhoenix <DzenPhoenix@gmail.com>
2021-01-17 13:53:52 +0200

    Finally

--- JS/17.01.21/assets/script/index.js
+++ JS/17.01.21/assets/script/index.js
@@ -77,6 +77,54 @@ function program_2() {
 
 //program_2();
 
+class Stack {
+
+    #size = 0;
+    #maxSize = 0;
+
+    constructor(maxSize = 1000) {
+        this.#size = 0;
+        this.#maxSize = maxSize;
+
+    }
+
+    get size() {
+        return this.#size;
+    }
+
+    get isEmpty() {
+        return this.#size === 0;
+    }
+
+    push(elem) {
+        if (this.#size === this.#maxSize) {
+            throw new RangeError("Stack is overflow");
+        }
+        this[`_${this.#size++}`] = elem;
+        return this.#size;
+    }
+
+    pop() {
+        if (this.isEmpty) {
+            return;
+        }
+        const value = this[--this.#size];
+        delete this[this.#size];
+        return value;
+    }
+
+    peek() {
+        if (this.isEmpty) {
+            return;
+        }
+        return this[this.#size - 1];
+    }
+
+    [Symbol.iterator]() {
+        return new StackIterator(this);
+    }
+}
+
 function program_3() {
 
     function find(arr, value) {
@@ -92,57 +140,237 @@ function program_3() {
     const idx = find(arr, 8);
     console.log(idx);
 
-    class Stack {
 
-        #size = 0;
-        #maxSize = 0;
-
-        constructor(maxSize = 1000) {
-            this.#size = 0;
-            this.#maxSize = maxSize;
-
-        }
-
-        get size() {
-            return this.#size;
-        }
-
-        get isEmpty() {
-            return this.#size === 0;
-        }
-
-        push(elem) {
-            if (this.#size === this.#maxSize) {
-                throw new RangeError("Stack is overflow");
-            }
-            this[this.#size++] = elem;
-            return this.#size;
-        }
-
-        pop() {
-            if (this.isEmpty) {
-                return;
-            }
-            const value = this[--this.#size];
-            delete this[this.#size];
-            return value;
-        }
-
-        peek() {
-            if (this.isEmpty) {
-                return;
-            }
-            return this.#size - 1;
-        }
-    }
 
     const stack1 = new Stack(10);
     stack1.push(5);
     stack1.push(-89);
-    stack1.push({name:"test"})
+    stack1.push({ name: "test" })
 
-    //console.dir(stack1);
-    console.log(stack1.peek());
+    console.dir(stack1);
+    //console.log(stack1.pop());
+    //stack1.peek();
+    //console.log(stack1.pop());
 }
 
-program_3();
+//program_3();
+
+function program_4() {
+
+    function lightCheck(str, options = {
+        brackets: {
+            '(': ')',
+            '{': '}',
+            '[': ']',
+        }
+    }) {
+        const bracketStack = [];
+        const brackets = option.brackets;
+
+        for (const s of str) {
+            if (brackets[s]) {
+                bracketsStack.push(s);
+                continue;
+            }
+            if (Object.values(brackets).includes(s)) {
+                if (brackets[bracketsStack[bracketStack.length - 1] == s]) {
+                    bracketStack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return bracketStack.length == 0;
+    }
+
+
+}
+
+//program_4();
+
+function program_5() {
+    const vocabulary = new Map();
+    vocabulary.set('cat', "Kot");
+    vocabulary.set('dog', "Sobuka");
+    vocabulary.set('squirrel', "Belka");
+
+    const translate = str => str.split(" ").map(elem => vocabulary.get(elem)).join(" ");
+    const str = "cat dog squirrel";
+    console.log(translate(str));
+}
+
+//program_5();
+
+
+class StackIterator {
+    constructor(stack) {
+        this._stack = stack;
+        this._start = 0;
+    }
+
+    next() {
+        if (this._start >= this._stack.size) {
+            return {
+                value: undefined,
+                done: true,
+            }
+        }
+        return {
+            value: this._stack[`_${this._start}`],
+            done: this._start++ === this._stack.size,
+        };
+
+    }
+}
+/*const stack1 = new Stack(10);
+stack1.push(5);
+stack1.push(-89);
+stack1.push({ name: "test" })
+
+console.dir(stack1);*/
+
+class ListItem {
+    constructor(value, next = null, prev = null) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+class List {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    addBegin(value) {
+        const newHead = new ListItem(value, this.head);
+        if (this.head) {
+            this.head.prev = newHead;
+        }
+        this.head = newHead;
+        if (!this.tail) {
+            this.tail = newHead;
+        }
+        this.size++;
+    }
+
+    addEnd(value) {
+        const newTail = new ListItem(value, null, this.tail);
+        this.tail.next = newTail;
+        this.tail = newTail;
+        if (!this.head) {
+            this.head = newTail;
+        }
+        this.size++;
+    }
+
+    [Symbol.iterator]() {
+        let current = this.head;
+        return {
+            next() {
+                if (current) {
+                    const value = current.value;
+                    current = current.next;
+                    return {
+                        value: value,
+                        done: false
+                    }
+                }
+                else {
+                    return {
+                        done: true,
+                    }
+                }
+            }
+        }
+    }
+}
+
+//const list = new List();
+//console.dir( list );
+
+
+class MyArrayIterator {
+    constructor(myArr) {
+        this.myArr = myArr;
+        this.current = 0;
+    }
+    next() {
+        if (this.current >this.myArr.size-1) {
+            return {
+                value: null,
+                done: true,
+            }
+        }
+        else {
+            return {
+                value: this.myArr.getElem(this.current++),
+                done: false,
+            }
+        }
+
+    }
+}
+
+class MyArray {
+
+    size = 0;
+    constructor(maxSize = 100) {
+        this.maxSize = maxSize;
+    }
+
+    push(elem) {
+        if (this.size == this.maxSize) { throw new RangeError("Stack overflow"); }
+        this[this.size] = elem;
+        this.size++;
+    }
+
+    getElem(idx) {
+        if (idx > this.size - 1) {
+            throw new RangeError("Index out of boundary of array");
+        }
+        return this[idx];
+    }
+
+    [Symbol.iterator] = function () {
+        return new MyArrayIterator(this);
+    }
+}
+
+const arr = new MyArray();
+arr.push(11);
+arr.push(63);
+arr.push("asdasd");
+
+
+for (const elem of arr) {
+    console.log(elem);
+}
+
+class User{
+    constructor(name,surname){
+        this.name=name;
+        this.surname=surname;
+    }
+
+}
+
+class Student extends User(){
+    static Count =0;
+    constructor(name,surname,year){
+        super(name,surname);
+        this.year = year;
+        Student.Count++;
+    }
+
+    get FullName(){
+        return `${this.name} ${this.surname}`;
+    }
+
+    get Course(){
+        return new Date().getFullYear()-this.year;
+    }
+}
